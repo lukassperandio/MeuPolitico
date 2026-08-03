80a155ec294be56ecf554b82f87e9ff944af7710
“lukassperandio” <emanuelsperandio@gmail.com>
2026-08-03 15:30:34 +0100

    first commit

---
+++ meupolitico-api/meupolitico-api/src/main/java/com/meupolitico/MeupoliticoApiApplication.java
@@ -0,0 +1,13 @@
+package com.meupolitico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeupoliticoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeupoliticoApiApplication.class, args);
+	}
+
+}
---
+++ meupolitico-api/meupolitico-api/src/test/java/com/meupolitico/MeupoliticoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meupolitico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeupoliticoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
