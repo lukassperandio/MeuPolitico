b51876ae50ea5a385854415cb8ae7292e2ab6498
“lukassperandio” <emanuelsperandio@gmail.com>
2026-09-07 07:37:46 +0100

    fix: use pt-BR locale for currency and dates

--- meupolitico-web/src/app/app.config.ts
+++ meupolitico-web/src/app/app.config.ts
@@ -1,13 +1,17 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, LOCALE_ID } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
 
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+
+registerLocaleData(localePt);
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
-    provideHttpClient()
+    provideHttpClient(),
+    { provide: LOCALE_ID, useValue: 'pt-BR' }
   ]
 };
